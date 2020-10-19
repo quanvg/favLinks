b74337810f8754774fa49496712bf30afcd5a6ec
quanvg <vqa.swe@gmail.com>
2020-10-19 01:45:19 -0400

    add input fields event listeners and handler
    add onChange event listener to input fields
    
    handleChange: 1 event handler for both onChange events thanks to using ES6's objects' computed property [] and event.target.name; Handler update states based on user's inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,14 +10,36 @@ export default class Form extends Component {
     };
   }
 
+  handleChange = event => {
+    this.setState(
+      {
+        [event.target.name]: event.target.value,
+      },
+      () =>
+        console.log(
+          `form's state updated: name:${this.state.name}\nURL: ${this.state.URL}`,
+        ),
+    );
+  };
+
   render() {
     return (
       <Fragment>
         <form>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={this.state.name} />
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
           <label htmlFor="URL">URL</label>
-          <input type="text" name="URL" value={this.state.URL} />
+          <input
+            type="text"
+            name="URL"
+            value={this.state.URL}
+            onChange={this.handleChange}
+          />
         </form>
         <button>Submit</button>
       </Fragment>
